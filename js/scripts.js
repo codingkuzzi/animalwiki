51afe14abf84851d0738fad3f184be61785c150c
Anna Kuznetsova and Saul Artiga <saul@>
2017-06-01 13:21:18 -0700

    create first draft of animal wiki html css and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  
+  $("form#survey").submit(function(event){
+    event.preventDefault();
+    var animal = $("input:radio[name=animal]:checked").val();
+      if (animal === "fruitbat") {
+      $("#splashanimal").toggle();
+      $("#fruitbat-showing").toggle();
+    } else if (animal === "possum") {
+    $("#splashanimal").toggle();
+    $("#possum-showing").toggle();
+  } if (animal === "trashpanda") {
+    $("#splashanimal").toggle();
+    $("#trashpanda-showing").toggle();
+    }
+      console.log(animal);
+  });
 });
